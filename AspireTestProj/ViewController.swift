bff46c67ee173a319ee5c7bdb7a666bf0ef50949
Serhii Semenov <1SsaibotT1@gmail.com>
2021-04-16 22:01:48 +0300

    First creating project commit

---
+++ AspireTestProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AspireTestProj
+//
+//  Created by Serhii Semenov on 16.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
