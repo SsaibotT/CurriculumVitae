f38c9beb7f1ad9551b2c7d0b83e6582895b43d05
Serhii Semenov <1SsaibotT1@gmail.com>
2021-04-17 13:37:58 +0300

    Create baseURL configurator

---
+++ AspireTestProj/Data/Networking/Configuration/NetworkConfiguration.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkConfiguration.swift
+//  AspireTestProj
+//
+//  Created by Serhii Semenov on 16.04.2021.
+//
+
+import Foundation
+
+class NetworkConfiguration {
+
+    // MARK: - Computed properties
+    static public var baseURL: String {
+        return "https://api.github.com"
+    }
+}
